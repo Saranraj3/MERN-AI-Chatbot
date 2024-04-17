a25d91a98f539707256e886c707ded603a290a6f
Saranraj <www.saranrajcoder@gmail.com>
2024-04-17 23:00:34 +0530

    Some Packages Installed and Middlewares API Keys

--- server/index.js
+++ server/index.js
@@ -1,17 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const router = require('./Routers');
 require('dotenv').config();
-const {MONGODB_URL,PORT} = process.env;
+const { MONGODB_URL, PORT } = process.env;
 const app = express();
 
 mongoose
-  .connect(MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("MongoDB is  connected successfully"))
-  .catch((err) => console.error(err));
+    .connect(MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log("MongoDB is  connected successfully"))
+    .catch((err) => console.error(err));
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+    console.log(`Server is listening on port ${PORT}`);
 });
+
+//Middleawers
+app.use(express.json());
+app.use(morgan('dev'));
+app.use('/api/version/', router);
+
+
+
+
