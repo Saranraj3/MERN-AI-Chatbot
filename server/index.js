a04426e6d83204cbb761d38b1624d058ae28b75c
Saranraj <www.saranrajcoder@gmail.com>
2024-04-17 22:02:03 +0530

    MongoDB Successfully Connected

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+require('dotenv').config();
+const {MONGODB_URL,PORT} = process.env;
 const app = express();
 
-app.listen(()=>{
-    console.log('started')
-})
+mongoose
+  .connect(MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
