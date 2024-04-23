43b071a479f37dbe48bce57b0b4cc184c654fa02
Saranraj <www.saranrajcoder@gmail.com>
2024-04-23 07:41:02 +0530

    UserController Updated

--- server/Controllers/UserController.js
+++ server/Controllers/UserController.js
@@ -1,15 +1,75 @@
+import jwt from "jsonwebtoken";
 import User from "../Models/User.js";
 
-export const GetAllUserData = async (req, res, next) => {
+const login = async (req, res) => {
+    const { email, password } = req.body;
 
-    try {
-        const users = await User.find();
-        return res.status(200).json({ message: 'ok', users });
-    } catch (error) {
-        console.log(error);
-        return res.status(200).json({ message: 'error', cause: error.message });
-    };
+    if (!email || !password) {
+        return res.status(400).json({
+            msg: "Bad request. Please add email and password in the request body",
+        });
+    }
 
+    let foundUser = await User.findOne({ email: req.body.email });
+    if (foundUser) {
+        const isMatch = await foundUser.comparePassword(password);
+
+        if (isMatch) {
+            const token = jwt.sign(
+                { id: foundUser._id, name: foundUser.name },
+                process.env.JWT_SECRET,
+                {
+                    expiresIn: "30d",
+                }
+            );
+
+            return res.status(200).json({ msg: "user logged in", token });
+        } else {
+            return res.status(400).json({ msg: "Bad password" });
+        }
+    } else {
+        return res.status(400).json({ msg: "Bad credentails" });
+    }
 };
 
+const dashboard = async (req, res) => {
+    const luckyNumber = Math.floor(Math.random() * 100);
 
+    res.status(200).json({
+        msg: `Hello, ${req.user.name}`,
+        secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+};
+
+const getAllUsers = async (req, res) => {
+    let users = await User.find({});
+
+    return res.status(200).json({ users });
+};
+
+const register = async (req, res) => {
+    let foundUser = await User.findOne({ email: req.body.email });
+    if (foundUser === null) {
+        let { username, email, password } = req.body;
+        if (username.length && email.length && password.length) {
+            const person = new User({
+                name: username,
+                email: email,
+                password: password,
+            });
+            await person.save();
+            return res.status(201).json({ person });
+        } else {
+            return res.status(400).json({ msg: "Please add all values in the request body" });
+        }
+    } else {
+        return res.status(400).json({ msg: "Email already in use" });
+    }
+};
+
+export default {
+    login,
+    register,
+    dashboard,
+    getAllUsers,
+};
