3d300884741529d0a1d6d2d1c6f798bf6616a496
Saranraj <www.saranrajcoder@gmail.com>
2024-04-23 08:34:35 +0530

    Deleted Routers Folder

--- server/Routers/UserRouter.js
+++
@@ -1,7 +0,0 @@
-import { Router } from 'express';
-import { GetAllUserData } from '../Controllers/UserController.js';
-const UserRouter = Router();
-
-UserRouter.get('/', GetAllUserData)
-
-export default UserRouter;
