049380c1b7a4b013bfb82663c173e6fab22785ea
Saranraj <www.saranrajcoder@gmail.com>
2024-04-23 07:44:23 +0530

    index.js Filer Updated

--- server/index.js
+++ server/index.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import morgan from 'morgan';
-import Route from './Routers/index.js';
 import { config } from 'dotenv';
 const { PORT } = process.env;
 config();
@@ -15,15 +13,12 @@ mongoose
     .then(() => console.log("MongoDB is  connected successfully"))
     .catch((err) => console.error(err));
 
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
 });
 
 //Middleawers
 app.use(express.json());
-app.use(morgan('dev'));
-app.use('/api/version/', Route);
-
 
 
 
