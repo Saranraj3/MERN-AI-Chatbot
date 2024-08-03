684b2df9774931f1c431ecef0b35b25c1b5cacfa
Saranraj <www.saranrajcoder@gmail.com>
2024-08-03 10:46:41 +0530

    MongoDB Database Connected Successfully

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config()
 
 const app = express()
 
-app.listen(()=>{
-    console.log('Server Running')
+// MongoDB Database Connection
+
+mongoose.connect(process.env.MONGODB).then(() => {
+    console.log('MongoDB Connection Successfully Connected')
+}).catch((err) => {
+    console.log(err)
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Server Running Port 5010")
 })
