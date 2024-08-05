4c34053a6dfc9900a7cc9da00fe010b69abecb6c
Saranraj <www.saranrajcoder@gmail.com>
2024-08-05 16:54:40 +0530

    Signup Page Client Side and Server Side Finished

--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -1,25 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 function Signup() {
+
+    const [formdata, setFormdata] = useState({});
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const Handlechange = (e) => {
+        setFormdata({ ...formdata, [e.target.id]: e.target.value });
+    }
+    const Handlesumbit = async (e) => {
+        e.preventDefault()
+        try {
+            setLoading(true);
+            setError(false);
+            const res = await fetch('/api/auth/signup', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formdata),
+            });
+            const data = await res.json()
+            console.log(data);
+            setLoading(false)
+            if (data.success === false) {
+                setError(true);
+                return;
+            }
+        } catch (error) {
+            setLoading(false);
+            setError(true);
+        }
+    };
     return (
         <div className='min-h-screen  bg-gray-900'>
             <h1 className='pt-[3rem] font-Noto font-extrabold text-3xl cursor-pointer text-center text-white'>Register You're Account</h1>
-            <div>
+            <form onSubmit={Handlesumbit}>
                 <div className='mt-5 flex justify-center'>
-                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="text" placeholder='Name' required />
+                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="text" placeholder='Name' id='Name' required onChange={Handlechange} />
                 </div>
                 <div className='mt-3 flex justify-center'>
-                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="email" placeholder='Email' required />
+                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="email" placeholder='Email' id='Email' required onChange={Handlechange} />
                 </div>
                 <div className='mt-3 flex justify-center'>
-                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="password" placeholder='Password' required />
+                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="password" placeholder='Password' id='Password' required onChange={Handlechange} />
                 </div>
                 <div className='mt-3 flex justify-center'>
-                    <button type='submit' className='h-[2.5rem] w-[18rem] text-lg hover:opacity-80 rounded-sm bg-green-400'>Continue</button>
+                    <button disabled={loading} type='submit' className='h-[2.5rem] w-[18rem] text-lg hover:opacity-80 rounded-sm bg-green-400'>{loading ? 'Loading...' : 'Submit'}</button>
+                </div>
+                <div className='mt-2 font-bold flex justify-center text-red-500'>
+                    <p>{error && 'Something Went Wrong'}</p>
                 </div>
                 <span className='mt-3 flex justify-center'><p className='cursor-pointer text-white'>You Have an Account ?</p><Link to='/login' className='ml-2 cursor-pointer text-blue-400'>Login</Link></span>
-            </div>
+            </form>
         </div>
     )
 }
