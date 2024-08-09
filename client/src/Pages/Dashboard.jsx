d351dcdeab71246ad8ea93e661e874c1f0e7df2e
Saranraj <www.saranrajcoder@gmail.com>
2024-08-09 08:56:00 +0530

    Some Aligning Changes

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -3,7 +3,7 @@ import { FaBrain } from "react-icons/fa";
 import { IoMdCreate } from 'react-icons/io';
 import { IoBulbSharp } from "react-icons/io5";
 import { MdAccessTimeFilled } from "react-icons/md";
-
+import { FaArrowAltCircleUp } from "react-icons/fa";
 function Dashboard() {
   return (
     <div className='justify-center items-center h-full'>
@@ -14,8 +14,12 @@ function Dashboard() {
         <p className='pt-2 pb-2 pl-3 cursor-pointer border rounded-md w-[10rem]'> <MdAccessTimeFilled className='text-green-500' /> Time Managing For Productivity</p>
       </div>
       <div className='mt-[10rem]'>
-        <input className=' outline-none pl-4 w-[44rem] h-[2.5rem] text-black rounded-2xl' type="text" placeholder='Ask Anything You Need to Know' />
+        <input className='outline-none pl-4 w-[44rem] h-[3rem] text-white  bg-gray-600 rounded-3xl' autoFocus type="text" placeholder='Ask Anything You Need to Know' />
       </div>
+      <div className='mt-[-2.6rem] mr-5 cursor-pointer flex justify-end'>
+        <FaArrowAltCircleUp className='size-[2rem]' />
+      </div>
+      <p className='mt-5 font-Noto text-white text-center cursor-default'>This Chat AI Can Make Mistakes</p>
     </div>
   )
 }
