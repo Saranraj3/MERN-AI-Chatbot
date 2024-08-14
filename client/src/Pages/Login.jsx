48c6e85622b58ce829ece6bbaac9ea34a243ca56
Saranraj <www.saranrajcoder@gmail.com>
2024-08-14 14:14:11 +0530

    Login and Signup Pages Fully Responsive Design Completed

--- client/src/Pages/Login.jsx
+++ client/src/Pages/Login.jsx
@@ -35,27 +35,29 @@ function Login() {
         }
     };
     return (
-        <div className='min-h-screen bg-gray-900'>
-            <h1 className='pt-[3rem] font-Noto font-extrabold text-2xl cursor-pointer text-center text-white'>Welcome Back To You're Account </h1>
-            <form onSubmit={Handlesumbit}>
-                <div className='mt-5 flex justify-center'>
-                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="email" placeholder='Email' id='Email' required onChange={Handlechange} />
-                </div>
-                <div className='mt-3 flex justify-center'>
-                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="password" placeholder='Password' id='Password' required onChange={Handlechange} />
-                </div>
-                <div className='mt-3 flex justify-center'>
-                    <button disabled={loading} type='submit' className='h-[2.5rem] w-[18rem] text-lg hover:opacity-80 rounded-sm bg-green-400'>{loading ? 'Loading...' : 'Submit'}</button>
-                </div>
-                <div className='mt-3 flex justify-center'>
-                    <img className='cursor-pointer absolute size-[2.5rem]' src={googlelogo} alt="" />
-                    <button type='submit' className='h-[2.5rem] w-[18rem] text-lg rounded-sm bg-white'></button>
-                </div>
-                <div className='mt-2 font-bold flex justify-center text-red-500'>
-                    <p>{error && 'Something Went Wrong'}</p>
-                </div>
-                <span className='mt-3 flex justify-center'><p className='cursor-pointer text-white'>You Don't Have an Account ?</p><Link to='/signup' className='ml-2 cursor-pointer text-blue-400'> Singup</Link></span>
-            </form>
+        <div className='overflow-y-hidden min-h-screen bg-gray-900 flex items-center justify-center'>
+            <div>
+                <h1 className='xs:text-xl lg:text-2xl pt-[3rem] font-Noto font-extrabold cursor-pointer text-center text-white'>Welcome Back To You're Account </h1>
+                <form onSubmit={Handlesumbit}>
+                    <div className='mt-5 flex justify-center'>
+                        <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="email" placeholder='Email' id='Email' required onChange={Handlechange} />
+                    </div>
+                    <div className='mt-3 flex justify-center'>
+                        <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="password" placeholder='Password' id='Password' required onChange={Handlechange} />
+                    </div>
+                    <div className='mt-3 flex justify-center'>
+                        <button disabled={loading} type='submit' className='h-[2.5rem] w-[18rem] font-bold text-lg hover:opacity-80 rounded-sm bg-green-400'>{loading ? 'Loading...' : 'Submit'}</button>
+                    </div>
+                    <div className='mt-3 flex justify-center'>
+                        <img className='cursor-pointer absolute size-[2.5rem]' src={googlelogo} alt="" />
+                        <button type='submit' className='h-[2.5rem] w-[18rem] rounded-sm bg-white'></button>
+                    </div>
+                    <div className='mt-2 font-bold flex justify-center text-red-500'>
+                        <p>{error && 'Something Went Wrong'}</p>
+                    </div>
+                    <span className='mt-3 flex justify-center'><p className='cursor-pointer text-white'>You Don't Have an Account ?</p><Link to='/signup' className='ml-2 cursor-pointer text-blue-400'> Singup</Link></span>
+                </form>
+            </div>
         </div>
     )
 }
--- client/src/Pages/Signup.jsx
+++ client/src/Pages/Signup.jsx
@@ -35,26 +35,28 @@ function Signup() {
         }
     };
     return (
-        <div className='min-h-screen  bg-gray-900'>
-            <h1 className='pt-[3rem] font-Noto font-extrabold text-3xl cursor-pointer text-center text-white'>Register You're Account</h1>
-            <form onSubmit={Handlesumbit}>
-                <div className='mt-5 flex justify-center'>
-                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="text" placeholder='Name' id='Name' required onChange={Handlechange} />
-                </div>
-                <div className='mt-3 flex justify-center'>
-                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="email" placeholder='Email' id='Email' required onChange={Handlechange} />
-                </div>
-                <div className='mt-3 flex justify-center'>
-                    <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="password" placeholder='Password' id='Password' required onChange={Handlechange} />
-                </div>
-                <div className='mt-3 flex justify-center'>
-                    <button disabled={loading} type='submit' className='h-[2.5rem] w-[18rem] text-lg hover:opacity-80 rounded-sm bg-green-400'>{loading ? 'Loading...' : 'Submit'}</button>
-                </div>
-                <div className='mt-2 font-bold flex justify-center text-red-500'>
-                    <p>{error && 'Something Went Wrong'}</p>
-                </div>
-                <span className='mt-3 flex justify-center'><p className='cursor-pointer text-white'>You Have an Account ?</p><Link to='/login' className='ml-2 cursor-pointer text-blue-400'>Login</Link></span>
-            </form>
+        <div className='overflow-y-hidden min-h-screen bg-gray-900 flex items-center justify-center'>
+            <div>
+                <h1 className='xs:text-2xl lg:text-3xl  pt-[3rem] font-Noto font-extrabold cursor-pointer text-center text-white'>Register You're Account</h1>
+                <form onSubmit={Handlesumbit}>
+                    <div className='mt-5 flex justify-center'>
+                        <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="text" placeholder='Name' id='Name' required onChange={Handlechange} />
+                    </div>
+                    <div className='mt-3 flex justify-center'>
+                        <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="email" placeholder='Email' id='Email' required onChange={Handlechange} />
+                    </div>
+                    <div className='mt-3 flex justify-center'>
+                        <input className='pl-2 cursor-pointer font-bold font-Noto rounded-sm outline-none border-black h-[2.5rem] w-[18rem] border' type="password" placeholder='Password' id='Password' required onChange={Handlechange} />
+                    </div>
+                    <div className='mt-3 flex justify-center'>
+                        <button disabled={loading} type='submit' className='h-[2.5rem] w-[18rem] text-lg font-bold hover:opacity-80 rounded-sm bg-green-400'>{loading ? 'Loading...' : 'Submit'}</button>
+                    </div>
+                    <div className='mt-2 font-bold flex justify-center text-red-500'>
+                        <p>{error && 'Something Went Wrong'}</p>
+                    </div>
+                    <span className='mt-3 flex justify-center'><p className='cursor-pointer text-white'>You Have an Account ?</p><Link to='/login' className='ml-2 cursor-pointer text-blue-400'>Login</Link></span>
+                </form>
+            </div>
         </div>
     )
 }
