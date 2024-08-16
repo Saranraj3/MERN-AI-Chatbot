cea06874774ae694109cad184dd9e248dbe83338
Saranraj <www.saranrajcoder@gmail.com>
2024-08-16 17:49:46 +0530

    Dashboard Page Fully Responsived

--- client/src/Pages/Dashboard.jsx
+++ client/src/Pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { FaBrain } from "react-icons/fa";
 import { IoMdCreate } from 'react-icons/io';
 import { IoBulbSharp } from "react-icons/io5";
@@ -9,34 +9,45 @@ import { MdAccountCircle } from "react-icons/md";
 
 function Dashboard({ sidebarVisible }) {
   return (
-    <div className={`${sidebarVisible ? 'w-4/5' : 'w-full'} flex flex-col items-center justify-center h-full`}>
-      <div className='invisible flex justify-start ml-[-7rem]'>
-        <BiMenuAltLeft className='size-[2rem]' />
+    <div className={`${sidebarVisible ? 'w-full md:w-4/5' : 'w-full'} flex flex-col items-center min-h-screen transition-all duration-300 ease-in-out`}>
+      <div className='flex justify-between items-center w-full px-4'>
+        <BiMenuAltLeft className='xs:visible lg:invisible text-[2rem] cursor-pointer' />
+        <MdAccountCircle className='text-[2.3rem] cursor-pointer' />
       </div>
-      <div className='w-full flex justify-end mr-5 mt-[-2rem]'>
-        <MdAccountCircle className='size-[2rem]' />
+      <div className='mt-[-5rem] flex justify-center items-center h-screen'>
+        <div className='text-white gap-3 max-w-2xl lg:grid-cols-4 lg:grid flex flex-wrap justify-center'>
+          <p className='pt-2 pb-2 pl-3 cursor-pointer border rounded-md w-40 flex items-center'>
+            <FaBrain className='text-red-500 mr-2' /> Improve Your Knowledge
+          </p>
+          <p className='pt-2 pb-2 pl-3 cursor-pointer border rounded-md w-40 flex items-center'>
+            <IoMdCreate className='text-blue-500 mr-2' /> Write Anything
+          </p>
+          <p className='pt-2 pb-2 pl-3 cursor-pointer border rounded-md w-40 flex items-center'>
+            <IoBulbSharp className='text-yellow-500 mr-2' /> Tips and Tricks
+          </p>
+          <p className='pt-2 pb-2 pl-3 cursor-pointer border rounded-md w-40 flex items-center'>
+            <MdAccessTimeFilled className='text-green-500 mr-2' /> Time Managing For Productivity
+          </p>
+        </div>
       </div>
-      <div className='mt-[10rem] text-white gap-3 max-w-[50rem] flex flex-wrap justify-center'>
-        <p className='pt-2 pb-2 pl-3 cursor-pointer border rounded-md w-[10rem] flex items-center'>
-          <FaBrain className='text-red-500 mr-2' /> Improve Your Knowledge
+      <div className='mt-[-10rem] top-full sticky pb-9'>
+        <div className='flex justify-center w-full relative'>
+          <div className='relative xs:w-[20rem] sm:w-[35rem] md:w-[45rem] lg:w-[rem] w-full max-w-[em]'>
+            <input
+              className='outline-none pl-3 pr-10 w-full h-12 text-white bg-gray-600 rounded-3xl'
+              autoFocus
+              type="text"
+              placeholder='Ask Anything You Need to Know'
+            />
+            <FaArrowAltCircleUp
+              className='absolute top-1/2 right-3 transform -translate-y-1/2 text-white text-[1.8rem] cursor-pointer'
+            />
+          </div>
+        </div>
+        <p className='mt-2 font-Noto text-white text-center cursor-default'>
+          This Chat AI Can Make Mistakes
         </p>
-        <p className='pt-2 pb-2 pl-3 cursor-pointer border rounded-md w-[10rem] flex items-center'>
-          <IoMdCreate className='text-blue-500 mr-2' /> Write Anything
-        </p>
-        <p className='pt-2 pb-2 pl-3 cursor-pointer border rounded-md w-[10rem] flex items-center'>
-          <IoBulbSharp className='text-yellow-500 mr-2' /> Tips and Tricks
-        </p>
-        <p className='pt-2 pb-2 pl-3 cursor-pointer border rounded-md w-[10rem] flex items-center'>
-          <MdAccessTimeFilled className='text-green-500 mr-2' /> Time Managing For Productivity
-        </p>
       </div>
-      <div className='mt-[10rem] flex justify-center w-full'>
-        <input className='outline-none pl-4 w-[44rem] h-[3rem] text-white bg-gray-600 rounded-3xl' autoFocus type="text" placeholder='Ask Anything You Need to Know' />
-      </div>
-      <div className='mt-[-2.6rem] invisible cursor-pointer flex justify-end w-full'>
-        <FaArrowAltCircleUp className='size-[2rem]' />
-      </div>
-      <p className='mt-5 font-Noto text-white text-center cursor-default'>This Chat AI Can Make Mistakes</p>
     </div>
   );
 }
